0fca5c957c5d85659e9493125e8bf45c57dbe9a4
Francisco Ovares Rojas <82686857+fran1709@users.noreply.github.com>
2024-02-24 16:34:17 -0600

    update
    header basico implementado, requiere modificarse visualmente

--- src/components/gestion_usuarios/LoginAccForm.tsx
+++ src/components/gestion_usuarios/LoginAccForm.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { logOut, login} from '../redux/reducers/authSlice';
-import { RootState } from '../redux/store';
+import { logOut, login} from '../../redux/reducers/authSlice';
+import { RootState } from '../../redux/store';
 
 const LoginAccountForm: React.FC = () => {
   const [email, setEmail] = useState<string>('');
--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,15 @@ import './index.css'
 import { Provider } from 'react-redux'; // Importa Provider desde react-redux
 import store from './redux/store.ts'; // Importa el store de Redux
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import CreateAccountForm from './components/CreateAccForm.tsx';
-import LoginAccountForm from './components/LoginAccForm.tsx';
+import CreateAccountForm from './components/gestion_usuarios/CreateAccForm.tsx';
+import LoginAccountForm from './components/gestion_usuarios/LoginAccForm.tsx';
+import ToolbarHeader from './components/header/ToolbarHeader.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}> {/* Envuelve el componente Router(todos los componentes) con Provider y pasa el store */}
       <Router>
+        <ToolbarHeader /> {/* Colocando tu ToolbarHeader aquí para que se renderice una unica vez*/}
         <Routes>
           <Route path="/" element={<App/>} />
           <Route path="/iniciar-sesion" element={<LoginAccountForm />} />
