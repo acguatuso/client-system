8385501c72ab858f0915cd2249228082be2f05fd
mariartve <110705321+mariartve@users.noreply.github.com>
2024-05-07 17:35:46 -0600

    fixes

--- src/components/gestion_cursos/Card.tsx
+++ src/components/gestion_cursos/Card.tsx
@@ -1,32 +1,33 @@
-import { Link} from 'react-router-dom';
-import './CursosMain.css';
-import { FaArrowRight } from 'react-icons/fa';
-import { Curso } from './curso.interface';
+import { Link } from "react-router-dom";
+import "./CursosMain.css";
+import { FaArrowRight } from "react-icons/fa";
+import { Curso } from "./curso.interface";
 
 interface CardProps {
-    to: string;
-    curso: Curso
+  to: string;
+  curso: Curso;
 }
 
 function Card({ to, curso }: CardProps) {
-    return (
-        <div className={`card my-3 ${curso.visible === 2 ? 'custom-card-proximamente' : 'custom-card'}`}>
-            <div className="card-body">
-                <h5 className="card-title">{curso.nombre}</h5>
-                <img
-                    src={curso.download_url}
-                    alt="Imagen"
-                    className='card-img'
-                />
-            </div>
-            {curso.visible === 1 && (
-                <div className="card-footer">
-                    <Link to={to} state={{ curso }} className="read-more">Leer más <FaArrowRight /></Link>
-                </div>
-            )}
+  return (
+    <div
+      className={`card my-3 ${
+        curso.visible === 2 ? "custom-card-proximamente" : "custom-card"
+      }`}
+    >
+      <div className="card-body">
+        <h5 className="card-title">{curso.nombre}</h5>
+        <img src={curso.download_url} alt="Imagen" className="card-img" />
+      </div>
+      {curso.visible === 1 && (
+        <div className="card-footer">
+          <Link to={to} state={{ curso }} className="read-more">
+            Leer más <FaArrowRight />
+          </Link>
         </div>
-    );
+      )}
+    </div>
+  );
 }
-
 
 export default Card;
