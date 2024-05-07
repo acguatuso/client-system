86615382c1113546ac70a70da3b92756c5290922
David Gonzalez <davidega12@gmail.com>
2024-05-07 08:24:43 -0600

    About publico

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -3,11 +3,6 @@ import { AdsSection } from './components/AdsSection';
 import { fetchMainSection, fetchSections } from '../../redux/reducers/aboutSlice';
 import { useAppDispatch } from "../../hooks/hooks";
 import { useEffect } from "react";
-// import { useSelector } from "react-redux";
-// import { RootState } from "../../redux/store";
-import { useNavigate } from "react-router-dom";
-import { RootState } from "../../redux/store";
-import { useSelector } from "react-redux";
 
 export const About = () => {
   //IMPLEMENTACION DE REDUX
@@ -20,20 +15,6 @@ export const About = () => {
     })()
   }, [])
   
-  // LOGICA PARA REDIRECCIONAR SI NO SE ESTA LOGUEADO, PARA QUE NO SE PUEDA ACCEDER MENDIATE URL DIRECTA
-  // React-router-dom
-  const navigate = useNavigate();
-  //Redux Hooks & Access
-  const user = useSelector((state: RootState) => state.auth.user);
-  const loggedIn = useSelector((state: RootState) => state.auth.loggedIn);
-
-  //Redireccionar si está no logueado, y no hay usuario
-  useEffect(() => {
-    if (!loggedIn && !user) {
-      navigate("/");
-    }
-  }, [loggedIn, user, navigate]);
-
   return (
     <>
       <div className="p-3 mb-2 bg-white text-dark border" id="about-container">
