363b67ba0c64419b1ca02c0e82098d07f816e491
mariartve <110705321+mariartve@users.noreply.github.com>
2024-05-08 12:50:03 -0600

    Revert "Merge branch 'develop'"
    This reverts commit 1500253cc2b945228eaa6175d72ca1323b46f105, reversing
    changes made to ed973c18ea0a97afbd2428dcadf377c7f6180fc6.

--- src/components/gestion_cursos/Card.tsx
+++ src/components/gestion_cursos/Card.tsx
@@ -10,12 +10,16 @@ interface CardProps {
 
 function Card({ to, curso }: CardProps) {
   return (
-    <div className={`card rounded-4 my-3 ${curso.visible === 2 ? "custom-card-proximamente" : "custom-card"}`} >
-        <img src={curso.download_url} alt="Imagen" className="card-img-top rounded" style={{height: "70%"}}/>
-        <div className="card-body">
-            <h5 className="card-title my-0">{curso.nombre}</h5>
-        </div>
-        {curso.visible === 1 && (
+    <div
+      className={`card my-3 ${
+        curso.visible === 2 ? "custom-card-proximamente" : "custom-card"
+      }`}
+    >
+      <div className="card-body">
+        <h5 className="card-title">{curso.nombre}</h5>
+        <img src={curso.download_url} alt="Imagen" className="card-img" />
+      </div>
+      {curso.visible === 1 && (
         <div className="card-footer">
           <Link to={to} state={{ curso }} className="read-more">
             Leer más <FaArrowRight />
