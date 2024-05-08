301022b34174be84d7ffbfbd1aa66c83881469cb
Jose Serrano Quesada <joseserranoq00@gmail.com>
2024-05-07 20:43:30 -0600

    fix 2

--- src/redux/reducers/adsSlice.ts
+++ src/redux/reducers/adsSlice.ts
@@ -60,6 +60,18 @@ const adsSlice =  createSlice({
             state.loading = false
             state.error = action.error.message;            
         });
+        builder.addCase(fetchAds.pending, (state) => {
+            state.loading = true
+        });
+        builder.addCase(fetchAds.fulfilled, (state,action: PayloadAction<any[]>) => {
+            state.loading = false,
+            state.adsList = action.payload
+        });
+        builder.addCase(fetchAds.rejected, (state,action) => {
+            state.loading = false
+            state.adsList = [] as ads[],
+            state.error = action.error.message;            
+        });    
     }
 }
 )
