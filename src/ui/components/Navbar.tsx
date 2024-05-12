bf24b46039d7e69c1830f85f2f7c7aa55add0b75
Francisco Ovares Rojas <82686857+fran1709@users.noreply.github.com>
2024-05-11 19:51:37 -0600

    Develop (#9)
    * Cambios en Route: Versión de /client-system/
    
    * Fix visual del navbar Inicio
    
    * Sección de Mis Cursos realizada, falta testing
    
    * fix, testing done.
    
    * fix
    
    * to deploy fix
    
    * develop module
    
    * Descarga Logo en Firebase
    
    * Facebook Logo
    
    ---------
    
    Co-authored-by: David Gonzalez <davidega12@gmail.com>
    Co-authored-by: Jose Serrano Quesada <joseserranoq00@gmail.com>

--- src/ui/components/Navbar.tsx
+++ src/ui/components/Navbar.tsx
@@ -3,6 +3,9 @@ import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../../redux/store';
 import { logOut } from '../../redux/reducers/authSlice';
 import './Navbar.css';
+import { useEffect, useState } from 'react';
+import { ref, getDownloadURL }from 'firebase/storage';
+import { firebase_storage } from '../../firebase';
 
 export const Navbar = () => {
 
@@ -11,6 +14,23 @@ export const Navbar = () => {
     const user = useSelector((state: RootState) => state.auth.user);
     const loggedIn = useSelector((state: RootState) => state.auth.loggedIn);
     const empresaData = useSelector((state: RootState) => state.empresa.dataEmpresa);
+
+    const [logoUrl, setLogoUrl] = useState('');
+
+    useEffect(() => { 
+        (async () => {
+            const imageRef = ref(firebase_storage, 'Empresa/Logo/logo');
+            getDownloadURL(imageRef)
+                .then((url) => {
+                    setLogoUrl(url);
+                })
+                .catch((error) => {
+                    console.error('Error descargando el logo:', error);
+                });
+
+        })()
+    }, []);
+
 
     const navigate = useNavigate();
 
@@ -29,15 +49,18 @@ export const Navbar = () => {
                             className="navbar-brand"
                             to="/client-system/"
                         >
-                            <img src="src/assets/LogoUCAG-E3vVaZ5h.png" alt="Logo" width="110" height="80" />
+
+                            <img src={logoUrl} alt="Logo" width="110" height="80" />
+
 
                         </NavLink>)}
                     {!user && !loggedIn && (
                         <NavLink
                             className="navbar-brand"
                             to="/client-system/"
-                        >
-                            <img src="src/assets/LogoUCAG-E3vVaZ5h.png" alt="Bootstrap" width="110" height="80" />
+
+                            <img src={logoUrl} alt="Logo" width="110" height="80" />
+
 
                         </NavLink>)}
                         <h5 className="navbar-title-text d-none d-sm-inline-block">
